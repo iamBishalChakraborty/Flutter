05564dfc4c90dc1401f9bc13ca02e4298270075d
Bishal Chakraborty <cbishal6@gmail.com>
2020-06-29 21:55:45 +0530

    playing with Api/Json

--- lib/homepage/homePage.dart
+++ lib/homepage/homePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_learn/drawer.dart';
 import 'package:flutter_learn/homepage/changeNameCard.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,15 +13,29 @@ class _HomePageState extends State < HomePage > {
 
   TextEditingController _textEditingController = TextEditingController();
   var myText = "Change Me";
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
 
   @override
   void initState() {
     super.initState();
+      getData();
+  }
+
+  getData() async{
+    var response = await http.get(url);
+    data = jsonDecode(response.body);
+    setState(() {
+      
+    });
+
+
+
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold( 
       backgroundColor: Colors.grey[400],
       appBar: AppBar(
         title: Text('My New Application'),
@@ -28,11 +43,17 @@ class _HomePageState extends State < HomePage > {
       body: Container(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-            child: Card(
-              child: SingleChildScrollView(
-                child: ChangeNameCard(myText: myText, textEditingController: _textEditingController),
-              ),
-            ),
+            child: data != null ? 
+            ListView.builder(itemBuilder: (context, index){
+              return ListTile(
+                title: Text(data[index]["title"]),
+                subtitle: Text("ID : ${data[index]["id"]}"),
+                leading: Image.network(data[index]["url"]),
+              );
+            }, 
+            itemCount: data.length,
+            )
+             : Center(child: CircularProgressIndicator(),),
         ),
       ),
       floatingActionButton:
